64f0d7b0f1d4037026081dd435342b76c2d1be4a
Kazbek Dzarasov <kazbek_d@hotmail.com>
2025-03-31 19:30:49 +0200

    Created Robots Components

---
+++ src/CardList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <div> {
+      robots.map((robot, i) => {
+        const { id, name, email } = robot;
+        return (
+          <Card
+            key={i}
+            id={id}
+            name={name}
+            email={email} />
+        );
+      })
+    }
+    </div>
+  );
+}
+
+export default CardList;
+
